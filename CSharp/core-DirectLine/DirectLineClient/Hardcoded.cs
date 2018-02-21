54c0995b48f5f38b2601ab003a9f475917e2141f
Amit Shukla <amishu@microsoft.com>
2018-02-21 15:50:45 -0800

    putting the changes for the direct line code changes

---
+++ CSharp/core-DirectLine/DirectLineClient/Hardcoded.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DirectLineSampleClient
+{
+    public static class Hardcoded
+    {
+        public static Dictionary<string, string> appNamevsExeMapping = new Dictionary<string, string>()
+        {
+            {"visual studio",@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"},
+            {"vs",@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"},
+            {"msvc",@"C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv.exe"},
+            {"notepad", @"C:\Windows\System32\notepad.exe" },
+            {"ms paint", @"C:\Windows\System32\mspaint.exe" },
+            {"paint", @"C:\Windows\System32\mspaint.exe" },
+            {"microsoft paint", @"C:\Windows\System32\mspaint.exe" },
+            {"command prompt", @"C:\Windows\System32\cmd.exe" },
+            {"command window", @"C:\Windows\System32\cmd.exe" },
+            {"command", @"C:\Windows\System32\cmd.exe" },
+        };
+    }
+}
---
+++ CSharp/core-DirectLine/DirectLineClient/SpeechRecognition.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using Microsoft.CognitiveServices.SpeechRecognition;
+
+namespace DirectLineSampleClient
+{
+    public class SpeechRecognition
+    {
+        static MicrophoneRecognitionClient micClient;
+        static String finaltext = "";
+        static bool flag = true;
+        static bool timerStarted = false;
+        static DateTime previousTime;
+
+        static string GetAuthenticationUri()
+        {
+            return "";
+        }
+
+        static void OnMicrophoneStatus(object sender, MicrophoneEventArgs e)
+        {
+            Console.WriteLine("--- Microphone status change received by OnMicrophoneStatus() ---");
+            Console.WriteLine("********* Microphone status: {0} *********", e.Recording);
+            if (e.Recording)
+            {
+                Console.WriteLine("Please start speaking.");
+            }
+
+            Console.WriteLine();
+        }
+
+        static void OnPartialResponseReceivedHandler(object sender, PartialSpeechResponseEventArgs e)
+        {
+            //Console.WriteLine("--- Partial result received by OnPartialResponseReceivedHandler() ---");
+            Console.WriteLine("{0}", e.PartialResult);
+            //Console.WriteLine();
+            finaltext = e.PartialResult;
+            if (!timerStarted)
+            {
+                timerStarted = true;
+                previousTime = DateTime.Now;
+            }
+        }
+
+        static void OnMicShortPhraseResponseReceivedHandler(object sender, SpeechResponseEventArgs e)
+        {
+            //Console.WriteLine("--- OnMicShortPhraseResponseReceivedHandler ---");
+
+            // we got the final result, so it we can end the mic reco.  No need to do this
+            // for dataReco, since we already called endAudio() on it as soon as we were done
+            // sending all the data.
+            finaltext = e.PhraseResponse.Results[0].DisplayText;
+            //Console.WriteLine(finaltext);
+            flag = false;
+        }
+
+        static void OnConversationErrorHandler(object sender, SpeechErrorEventArgs e)
+        {
+            Console.WriteLine("--- Error received by OnConversationErrorHandler() ---");
+            Console.WriteLine("Error code: {0}", e.SpeechErrorCode.ToString());
+            Console.WriteLine("Error text: {0}", e.SpeechErrorText);
+            Console.WriteLine();
+        }
+        static void CreateMicrophoneRecoClient()
+        {
+            micClient = SpeechRecognitionServiceFactory.CreateMicrophoneClient(
+                SpeechRecognitionMode.ShortPhrase,
+                "en-Us",
+                "b43e6e21b35b47ada3dce89cf362605e");
+            micClient.AuthenticationUri = GetAuthenticationUri();
+
+            // Event handlers for speech recognition results
+            micClient.OnMicrophoneStatus += OnMicrophoneStatus;
+            micClient.OnPartialResponseReceived += OnPartialResponseReceivedHandler;
+            micClient.OnResponseReceived += OnMicShortPhraseResponseReceivedHandler;
+            micClient.OnConversationError += OnConversationErrorHandler;
+        }
+        public static string start()
+        {
+            flag = true;
+            CreateMicrophoneRecoClient();
+            micClient.StartMicAndRecognition();
+
+            while (flag) ;
+
+            micClient.EndMicAndRecognition();
+            micClient.Dispose();
+
+
+            return finaltext;
+        }
+    }
+}
